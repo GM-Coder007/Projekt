6ebee6cbbd7cf79f41f3656186ce53b63b20e794
GM-Coder007 <91761395+GM-Coder007@users.noreply.github.com>
2022-05-24 18:53:00 +0200

    Update Register.js

--- gso-react/src/components/Register.js
+++ gso-react/src/components/Register.js
@@ -1,20 +1,19 @@
 import { useState } from 'react';
 
 function Register() {
-    const [username, setUsername] = useState([]);
-    const [password, setPassword] = useState([]);
+   
     const [email, setEmail] = useState([]);
+    const [password, setPassword] = useState([]);
     const [error, setError] = useState([]);
 
     async function Register(e){
         e.preventDefault();
-        const res = await fetch("http://localhost:3001/users", {
+        const res = await fetch("http://localhost:3001/user/register", {
             method: 'POST',
             credentials: 'include',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
                 email: email,
-                username: username,
                 password: password
             })
         });
@@ -33,7 +32,6 @@ function Register() {
     return(
         <form onSubmit={Register}>
             <input type="text" name="email" placeholder="Email" value={email} onChange={(e)=>(setEmail(e.target.value))} /> <br/> <br/>
-            <input type="text" name="username" placeholder="Uporabniško ime" value={username} onChange={(e)=>(setUsername(e.target.value))}/> <br/> <br/>
             <input type="password" name="password" placeholder="Geslo" value={password} onChange={(e)=>(setPassword(e.target.value))} /> <br/> <br/>
             <input class="btn btn-success" type="submit" name="submit" value="Registracija" />
             <label>{error}</label>
