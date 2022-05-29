5a51c5d96e919dd85be9e12a35b7ef7844f80d69
GM-Coder007 <91761395+GM-Coder007@users.noreply.github.com>
2022-05-29 20:43:23 +0200

    Update voznjaRoutes.js

--- gso-express/src/routes/voznjaRoutes.js
+++ gso-express/src/routes/voznjaRoutes.js
@@ -3,17 +3,18 @@ var router = express.Router();
 var voznjaController = require('../controllers/voznjaController.js');
 
 // GET
-router.get('/', voznjaController.list);
-router.get('/voznja', voznjaController.show);
-router.get('/vseVoznje', voznjaController.showVoznje);
+router.get('/', voznjaController.list); // vse vožnje vseh uporabnikov
+// router.get('/voznja', voznjaController.show); // ena vožnja
+router.get('/:id', voznjaController.show); // ena vožnja
+router.get('/vseVoznje', voznjaController.showVoznje); // vse vožnje prijavljenega uporabnika
 //router.get('/mojeVoznje', voznjaController.showMojeVoznje);
-router.get('/ustvariVoznjo', voznjaController.ustvariVoznjo);
+router.get('/ustvariVoznjo', voznjaController.ustvariVoznjo); // nalaganje pogleda za ustvarjanje vožnje 
 
 // POST
-router.post('/', voznjaController.create);
+router.post('/', voznjaController.create); // ustvarjanje vožnje
 
 
 // DELETE
-router.post('/izbrisi', voznjaController.izbrisi);
+router.post('/izbrisi/:id', voznjaController.izbrisi); // izbris vožnje
 
 module.exports = router;
